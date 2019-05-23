c2fb09bab973cf703a78f84e5a866dcfaba61f4a
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-23 18:22:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,10 +20,12 @@ end
 
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears)"
 end
 
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
